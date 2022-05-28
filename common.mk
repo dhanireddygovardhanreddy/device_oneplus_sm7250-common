2c608bf5c5f98e8a86fedb759bd8bc78bf34dfba
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-28 10:29:35 +0200

    sm7250-common: Stop explicitely building netutils-wrapper-1.0
    * Built by default
    
    Change-Id: Id6666535bb8d0b84d2d0a8d19411fafa49df3d68
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- common.mk
+++ common.mk
@@ -360,10 +360,6 @@ PRODUCT_PACKAGES += \
     libcodec2_hidl@1.0.vendor:32 \
     libcodec2_vndk.vendor:32
 
-# Net
-PRODUCT_PACKAGES += \
-    netutils-wrapper-1.0
-
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2 \
